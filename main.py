f0a9cf31165c5959b60e5a24d6c9c7b29dd2f25e
Davor Telisman <davortelisman@gmail.com>
2022-09-21 11:35:45 +0200

    Open are dnn model (list and condition of capture objects)

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import cv2
+# open dnn
+net =cv2.dnn.readNet("dnn_model/yolov4-tiny.weights","dnn_model/yolov4-tiny.cfg")
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(size = (320,320),scale = 1/255)
 
+# Set camera
 cap = cv2.VideoCapture(0)
-
 while True:
+
+    # Get frame
     rat, frame = cap.read()
+
+
+
     cv2.imshow("Frame",frame)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) == ord("q"):
+        break
